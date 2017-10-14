3e8780e76397badf37fec2e2e3cc51b011bbd505
MITESH PATEL <mitforu@gmail.com>
2017-10-14 12:19:18 -0500

    fixing port issue

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.listen(port);
 console.log('todo list RESTful API server started on: ' + port);*/
 
 
-/*
+
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
@@ -38,7 +38,7 @@ var port = process.env.PORT
 if(port == undefined) port=3000
 
 app.listen(port);
-*/
+
 
 
 
@@ -60,47 +60,6 @@ app.listen(port, function () {
   console.log('Example app listening on port ' + port);
 });*/
 
-
-
-
-
-
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
-var port = 8081
-
-// Create a server
-http.createServer( function (request, response) {  
-   // Parse the request containing file name
-   var pathname = url.parse(request.url).pathname;
-   
-   port = process.env.PORT || 8081
-   console.loh("------------------getting port" + port)
-
-   // Print the name of the file for which request is made.
-   console.log("Request for " + pathname + " received.");
-   
-   // Read the requested file content from file system
-   fs.readFile(pathname.substr(1), function (err, data) {
-      if (err) {
-         console.log(err);
-         // HTTP Status: 404 : NOT FOUND
-         // Content Type: text/plain
-         response.writeHead(404, {'Content-Type': 'text/html'});
-      }else {	
-         //Page found	  
-         // HTTP Status: 200 : OK
-         // Content Type: text/plain
-         response.writeHead(200, {'Content-Type': 'text/html'});	
-         
-         // Write the content of the file to response body
-         response.write(data.toString());		
-      }
-      // Send the response body 
-      response.end();
-   });   
-}).listen(process.env.PORT);
 
 // Console will print the message
 //console.log('Server running at http://127.0.0.1:8081/');

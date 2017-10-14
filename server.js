37de5ea4e39b269c2b8f7a1ea6cf03f5ad3df397
MITESH PATEL <mitforu@gmail.com>
2017-10-14 11:59:41 -0500

    adding web page support

--- server.js
+++ server.js
@@ -25,6 +25,8 @@ app.listen(port);
 
 console.log('todo list RESTful API server started on: ' + port);*/
 
+
+/*
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
@@ -32,17 +34,21 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 var routes = require('./api/routes/todoListRoutes'); //importing route
 routes(app); //register the route
+var port = process.env.PORT 
+if(port == undefined) port=3000
+
+app.listen(port);
+*/
+
+
+
+
+
 /*app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 */
 
-
-
- var port = process.env.PORT 
- if(port == undefined) port=3000
-
-app.listen(port);
 
 /*
 app.get('/person', function (req, res) {
@@ -53,3 +59,49 @@ app.get('/person', function (req, res) {
 app.listen(port, function () {
   console.log('Example app listening on port ' + port);
 });*/
+
+
+
+
+
+
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+
+// Create a server
+http.createServer( function (request, response) {  
+   // Parse the request containing file name
+   var pathname = url.parse(request.url).pathname;
+   
+   // Print the name of the file for which request is made.
+   console.log("Request for " + pathname + " received.");
+   
+   // Read the requested file content from file system
+   fs.readFile(pathname.substr(1), function (err, data) {
+      if (err) {
+         console.log(err);
+         // HTTP Status: 404 : NOT FOUND
+         // Content Type: text/plain
+         response.writeHead(404, {'Content-Type': 'text/html'});
+      }else {	
+         //Page found	  
+         // HTTP Status: 200 : OK
+         // Content Type: text/plain
+         response.writeHead(200, {'Content-Type': 'text/html'});	
+         
+         // Write the content of the file to response body
+         response.write(data.toString());		
+      }
+      // Send the response body 
+      response.end();
+   });   
+}).listen(8081);
+
+// Console will print the message
+console.log('Server running at http://127.0.0.1:8081/');
+
+
+
+
+
